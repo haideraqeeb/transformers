3b73369c7f34d29a1e818f4b680e0611b7e0f766
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-07 19:10:23 +0530

    add transformer block using self attention

--- modules.py
+++ modules.py
@@ -95,3 +95,42 @@ class SelfAttention(nn.Module):
         out = out.transpose(1, 2).contiguous().views(b, t, s * h)
 
         return self.unifyheads(out)
+    
+class TransformerBlock(nn.Module):
+    """
+    Straightforward transfomer module
+    """
+
+    def __init__(self, emb, heads, mask, seq_length, ff_hidden_mult=4, dropout=0.0,
+                sa_kwargs={}):
+        
+        super.__init__()
+        self.attention = SelfAttention(emb, heads=heads, mask=mask, **sa_kwargs)
+        self.mask = mask
+
+        self.norm1 = nn.LayerNorm(emb)
+        self.norm2 = nn.LayerNorm(emb)
+
+        self.ff = nn.Sequential(
+            nn.Linear(emb, ff_hidden_mult * emb),
+            nn.ReLU(),
+            nn.Linear(ff_hidden_mult * emb, emb)
+        )
+
+        self.drop = nn.Dropout(dropout)
+    
+    def forward(self, x):
+
+        attended = self.attention(x)
+
+        x = self.norm1(attended + x)
+
+        x = self.drop(x)
+
+        fedforward = self.ff(x)
+
+        x = self.norm2(fedforward + x)
+
+        x = self.drop(x)
+
+        return x
